e3904be24ca874043d5f9f98781bd91424812d76
BenDemandt <79916437+BenDemandt@users.noreply.github.com>
2021-05-31 21:20:33 +0200

    Add files via upload

---
+++ serverUDP.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+int packets = 0;
+int main(void)
+
+ {
+    int socket_desc;
+    struct sockaddr_in server_addr, client_addr;
+    char server_message[100], client_message[100];
+    int client_struct_length = sizeof(client_addr);
+    
+    
+    memset(server_message, '\0', sizeof(server_message));
+    memset(client_message, '\0', sizeof(client_message));
+    
+    // creeert socket
+    socket_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    
+    if(socket_desc < 0){
+        printf("error met aanmaken socket\n");
+        return -1;
+    }
+    
+  
+    // stelt poort en ip in:
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(24042);
+    server_addr.sin_addr.s_addr = inet_addr(NULL);
+    
+    // bindt de poort en ip
+    if(bind(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
+        printf("poort niet gevonden\n");
+        return -1;
+    }
+    
+		do
+		{
+			
+			printf("wachten op bericht\n\n");
+			// ontvangt bericht
+			if (recvfrom(socket_desc, client_message, sizeof(client_message), 0,
+			(struct sockaddr*)&client_addr, &client_struct_length) < 0){
+			printf("niks ontvangen\n");
+			return -1;
+			}
+			printf("bericht ontvangen van ip: %s en poort: %i\n",
+			inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+			printf("bericht van client: %s\n", client_message);
+           packets++;
+		   printf("aantal packets %d\n", packets);
+    
+			// geeft een respons
+			strcpy(server_message, client_message);
+	
+    
+			if (sendto(socket_desc, server_message, strlen(server_message), 0,
+			(struct sockaddr*)&client_addr, client_struct_length) < 0)
+			{	 
+			printf("kan niet verzenden\n");
+			return -1;
+			}
+		}while(1);
+    
+	
+	
+    // sluit het socket
+    close(socket_desc);
+   
+ }
+
